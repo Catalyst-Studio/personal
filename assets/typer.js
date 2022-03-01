d13484f058e7b4e9c6a14513f308fd816fa5e533
Catalyst-Craft <92801262+Catalyst-Craft@users.noreply.github.com>
2022-03-01 12:40:04 -0600

    Add files via upload

--- assets/typer.js
+++ assets/typer.js
@@ -1,8 +1,5 @@
-const typedTextSpan = document.querySelector(".typed-text");
-const cursorSpan = document.querySelector(".cursor");
-
-const textArray = ["Full Stack Developer","Front-End Specialist","Java Developer","Designer","Editor"];
-const typingDelay = 200;
+const textArray = ["Full Stack Developer","Front-End Specialist","Java Developer","Designer","Editor","15 year old kid!"];
+const typingDelay = 100;
 const erasingDelay = 100;
 const newTextDelay = 1000; // Delay between current and next text
 let textArrayIndex = 0;
@@ -10,26 +7,26 @@ let charIndex = 0;
 
 function type() {
   if (charIndex < textArray[textArrayIndex].length) {
-    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    if(!document.querySelector(".cursor-text").classList.contains("typing")) document.querySelector(".cursor-text").classList.add("typing");
+    document.querySelector(".typed-text").textContent += textArray[textArrayIndex].charAt(charIndex);
     charIndex++;
     setTimeout(type, typingDelay);
   } 
   else {
-    cursorSpan.classList.remove("typing");
+    document.querySelector(".cursor-text").classList.remove("typing");
   	setTimeout(erase, newTextDelay);
   }
 }
 
 function erase() {
 	if (charIndex > 0) {
-    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    if(!document.querySelector(".cursor-text").classList.contains("typing")) document.querySelector(".cursor-text").classList.add("typing");
+    document.querySelector(".typed-text").textContent = textArray[textArrayIndex].substring(0, charIndex-1);
     charIndex--;
     setTimeout(erase, erasingDelay);
   } 
   else {
-    cursorSpan.classList.remove("typing");
+    document.querySelector(".cursor-text").classList.remove("typing");
     textArrayIndex++;
     if(textArrayIndex>=textArray.length) textArrayIndex=0;
     setTimeout(type, typingDelay + 1100);
